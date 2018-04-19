a5ec84e6d414a594632f289058781cda23ace4bb
sarleone <sarah.m.leone@gmail.com>
2018-04-18 23:21:06 -0500

    added |

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -6,7 +6,7 @@ const NavBar = props => (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary navbar fixed-top">
         <p className="navbar-brand" href="#">Clicky Game!</p>
         <p className="nav-link-center"> Click an image to begin! </p>
-        <p className="nav-link-right"> Score: <span>{props.count}</span>  Top Score: <span>{props.topCount}</span></p>
+        <p className="nav-link-right"> Score: <span>{props.count}</span> |  Top Score: <span>{props.topCount}</span></p>
     </nav>
 );
 
